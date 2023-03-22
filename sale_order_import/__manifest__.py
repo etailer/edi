b6b8e7eb776be865764894aa83b0d296fad2eff7
Robert Coleman <github@robert.net.nz>
2023-03-22 21:26:05 +1300

    [MIG] sale_order_import: Migration to 16.0

--- sale_order_import/__manifest__.py
+++ sale_order_import/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Order Import",
-    "version": "14.0.1.4.1",
+    "version": "16.0.1.0.0",
     "category": "Sales Management",
     "license": "AGPL-3",
     "summary": "Import RFQ or sale orders from files",
