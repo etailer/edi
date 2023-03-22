8a6cd69006aea30d6a1aee28f75370abe5fabb75
Robert Coleman <github@robert.net.nz>
2023-03-22 21:25:33 +1300

    [MIG] edi_exchange_template_party_data: Migration to 16.0

--- edi_exchange_template_party_data/__manifest__.py
+++ edi_exchange_template_party_data/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "EDI Exchange Template - Party data",
     "summary": """Glue module betweeb edi_exchange_template and edi_party_data""",
-    "version": "14.0.1.0.1",
+    "version": "16.0.1.0.0",
     "development_status": "Alpha",
     "license": "AGPL-3",
     "author": "Camptocamp,Odoo Community Association (OCA)",
